67f4140365538a79b0a49518a64949de5244ac94
Tim <tim.yukhnov@gmail.com>
2026-01-24 22:59:34 +0300

    solve hw 1

---
+++ HW_1/dags/json_to_table.py
@@ -0,0 +1,66 @@
+from airflow.sdk import DAG
+from datetime import datetime, timedelta
+import json
+import os
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def json_to_table():
+    file_path = "/opt/airflow/data/pets-data.json"
+
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    pets = data['pets']
+
+    pet_rows = []
+    food_rows = []
+    for i, pet in enumerate(pets):
+        pet_id = i
+        name = pet.get("name")
+        species = pet.get("species")
+        fav_foods = pet.get("favFoods", [])
+        birth_year = pet.get("birthYear")
+        photo_url = pet.get("photo")
+        pet_rows.append((pet_id, name, species, birth_year, photo_url))
+        for food in fav_foods:
+            food_rows.append((pet_id, food))
+
+    pg_hook = PostgresHook(postgres_conn_id="postgres")
+
+    create_pets_table_sql = """
+    CREATE TABLE IF NOT EXISTS pets (
+        pet_id INT PRIMARY KEY,
+        name TEXT,
+        species TEXT,
+        birth_year INT,
+        photo_url TEXT
+    );
+    """
+
+    create_foods_table_sql = """
+        CREATE TABLE IF NOT EXISTS fav_foods (
+            pet_id INT,
+            food TEXT,
+            PRIMARY KEY (pet_id, food),
+            FOREIGN KEY (pet_id) REFERENCES pets(pet_id) ON DELETE CASCADE
+        );
+        """
+
+    pg_hook.run(create_pets_table_sql)
+    pg_hook.run(create_foods_table_sql)
+
+    pg_hook.insert_rows(table="pets", rows=pet_rows)
+    pg_hook.insert_rows(table="fav_foods", rows=food_rows)
+
+
+
+with DAG('extract_json', schedule=None, start_date=datetime(2026, 1, 1), catchup=False) as dag:
+    history = PythonOperator(
+        task_id="json_to_table",
+        python_callable=json_to_table
+    )
+
+
---
+++ HW_1/dags/xml_to_table.py
@@ -0,0 +1,91 @@
+from airflow.sdk import DAG
+from datetime import datetime, timedelta
+import json
+import os
+import xml.etree.ElementTree as ET
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def xml_to_table():
+    file_path = "/opt/airflow/data/nutrition.xml"
+
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    food_rows = []
+    for i, food in enumerate(root.findall('food')):
+        food_id = i
+        name = food.find('name').text
+        mfr = food.find('mfr').text
+
+        serving = food.find('serving').text
+        serving_units = food.find('serving').get('units')
+        calories_total = food.find('calories').get('total')
+        calories_fat = food.find('calories').get('fat')
+
+        total_fat = food.find('total-fat').text
+        saturated_fat = food.find('saturated-fat').text
+        cholesterol = food.find('cholesterol').text
+        sodium = food.find('sodium').text
+        carb = food.find('carb').text
+        fiber = food.find('fiber').text
+        protein = food.find('protein').text
+
+        vitamin_a = food.find('vitamins').find('a').text
+        vitamin_c = food.find('vitamins').find('c').text
+
+        mineral_ca = food.find('minerals').find('ca').text
+        mineral_fe = food.find('minerals').find('fe').text
+
+        food_rows.append((
+            food_id, name, mfr, serving, serving_units,
+            calories_total, calories_fat, total_fat, saturated_fat, cholesterol, sodium,
+            carb, fiber, protein, vitamin_a, vitamin_c, mineral_ca, mineral_fe
+        ))
+
+    create_foods_table_sql = """
+        CREATE TABLE IF NOT EXISTS foods (
+            id INT PRIMARY KEY,
+            name TEXT,
+            mfr TEXT,
+            serving FLOAT,
+            serving_units TEXT,
+            calories_total INT,
+            calories_fat INT,
+            total_fat FLOAT,
+            saturated_fat FLOAT,
+            cholesterol FLOAT,
+            sodium FLOAT,
+            carbs FLOAT,
+            fiber FLOAT,
+            protein FLOAT,
+            vitamin_a FLOAT,
+            vitamin_c FLOAT,
+            mineral_ca FLOAT,
+            mineral_fe FLOAT
+        );
+        """
+    pg_hook = PostgresHook(postgres_conn_id="postgres")
+
+    pg_hook.run(create_foods_table_sql)
+
+    pg_hook.insert_rows(
+          table="foods",
+          rows=food_rows,
+          target_fields=[
+              'id', 'name', 'mfr', 'serving', 'serving_units',
+              'calories_total', 'calories_fat', 'total_fat', 'saturated_fat' ,'cholesterol', 'sodium',
+              'carbs', 'fiber', 'protein', 'vitamin_a', 'vitamin_c', 'mineral_ca', 'mineral_fe'
+          ]
+      )
+
+
+with DAG('extract_xml', schedule=None, start_date=datetime(2026, 1, 1), catchup=False) as dag:
+    history = PythonOperator(
+        task_id="xml_to_table",
+        python_callable=xml_to_table
+    )
+
+
